1d801e55794ac81f452a6580c7308f860828809a
jackkyyang <furryyang@gmail.com>
2023-11-26 00:34:25 +0800

    更改bit map格式和译码表翻译，增加可读性

--- misc/python/decode.py
+++ misc/python/decode.py
@@ -3,6 +3,7 @@ from pprint import pprint
 from tomlkit.toml_file import TOMLFile
 from os import path
 from typing import Union
+from re import search
 
 def toml_parse(file_path:str) -> dict[str,Union[str,dict]]:
     to_doc = TOMLFile(file_path).read()
@@ -10,36 +11,55 @@ def toml_parse(file_path:str) -> dict[str,Union[str,dict]]:
 
 
 class Field():
-    def __init__(self,name:str,bit_pos:list[int],offset:int=0) -> None:
-        self._len = len(bit_pos)
+    def __init__(self,name:str,bit_pos:list[str],offset:int=0) -> None:
         self._name = name
         self._bit_pos = bit_pos
         self._offset = offset
-        self._mask_gen()
+        self.__len_calc()
+        self.__mask_gen()
+
+    def __len_calc(self):
+        self._len = 0
+        for pos in self._bit_pos:
+            search_result = search("(\d+):(\d+)|(\d+)",pos)
+            if search_result is not None:
+                if (search_result.groups()[2] is None):
+                    self._len += abs(int(search_result.groups()[0]) -
+                                     int(search_result.groups()[1]) + 1)
+                else:
+                    self._len += 1
+            else:
+                raise Exception("Format Error! Cannot parse the information of the field")
 
     def __len__(self):
         return self._len
 
-    def _mask_gen(self):
-        # mask 是 mask_shift 组成的对
-        prev_pos = self._bit_pos[0] - 1
-        tmp_mask = ["0"] * 32
+    def __mask_gen(self):
         offset = 0
-        self._mask_list = []
-
-        for i in range(self._len):
-            if self._bit_pos[i] != prev_pos + 1:
-                tmp_mask.reverse()
-                self._mask_list.append((offset,"".join(tmp_mask),))
-                offset = i
+        self._mask_list= []
+        for pos in self._bit_pos:
+            search_result = search("(\d+):(\d+)|(\d+)",pos)
+            if search_result is not None:
                 tmp_mask = ["0"] * 32
-
-            tmp_mask[self._bit_pos[i]] = "1"
-            prev_pos = self._bit_pos[i]
-
-            if i == self._len -1:
+                if (search_result.groups()[2] is None):
+                    end = max(int(search_result.groups()[0]),
+                              int(search_result.groups()[1]))
+                    start = min(int(search_result.groups()[0]),
+                                int(search_result.groups()[1]))
+                    len = end - start + 1
+                    for i in range(start,end + 1):
+                        tmp_mask[i] = "1"
+                else:
+                    bit_pos = int(search_result.groups()[2])
+                    tmp_mask[bit_pos] = "1"
+                    len = 1
                 tmp_mask.reverse()
                 self._mask_list.append((offset,"".join(tmp_mask),))
+                offset += len
+            else:
+                raise Exception("Format Error! Cannot parse the information of the field")
+
+
 
 
 
@@ -93,8 +113,9 @@ if __name__ == "__main__":
     # decoder.print_table()
     # decoder.print_arch_info()
 
-    rd = Field("rd",[7, 8, 9, 10, 11],0)
+    rd = Field("rd",["11:7"],0)
+    print(len(rd))
     print(rd._mask_list)
-
-    imm = Field("rd", [8,9,10,11,25,26,27,28,29,30,7,31],1)
+    imm = Field("rd",["11:8","30:25","7","31"],0)
+    print(len(imm))
     print(imm._mask_list)
